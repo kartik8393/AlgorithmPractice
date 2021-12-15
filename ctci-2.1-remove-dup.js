6197a2ff6eeae014e2d7bc99c6c8adf4c7dd3554
Kartik <kartikwakchaure@gmail.com>
2021-12-16 01:28:48 +0530

    16-12-2021

---
+++ ctci-2.1-remove-dup.js
@@ -0,0 +1,143 @@
+class Node{
+    constructor(data,next=null){
+        this.data=data;
+        this.next=next;
+    }
+}
+
+class LinkedList{
+    constructor(){
+        this.head=null;
+        this.size=0;
+    }
+
+    // INSERT FIRST NODE
+    insertFirst(data){
+        this.head=new Node(data,this.head)
+        this.size++;
+    } 
+
+    // INSERT LAST NODE
+    insertLast(data){
+        if(!this.head){
+            this.head=new Node(data)
+        }
+        else{
+            let current=this.head;
+            while(current.next){
+                current=current.next;
+            }
+            current.next=new Node(data)
+        }
+        this.size++;
+    }
+
+    // INSERT AT AN INDEX
+    insertIndex(data,index){
+        if(index==0){
+            this.insertFirst(data);
+            return;
+        }
+        if(index>this.size){
+            return false;
+        }
+
+        let current,previous;
+        current=this.head
+        let count=0;
+        while(count<index){
+            previous=current;
+            current=current.next;
+            count++;
+        }
+        let node=new Node(data)
+        previous.next=node;
+        node.next=current;
+        this.size++;
+        
+    }
+
+    // GET AT INDEX
+    getAt(index){
+        let current=this.head;
+        let currentIndex=0;
+        if(index>this.size) return false;
+        while(current){
+            if(currentIndex==index){
+                console.log(index,current.data)
+                return current.data;
+            }
+            current=current.next;
+            currentIndex++;
+        }
+    }
+
+    // REMOVE AT INDEX
+    removeAt(index){
+        if(index>this.size){
+            return;
+        }
+        if(index==0){
+            this.head=this.head.next;
+            this.size--;
+            return;
+        }
+
+        let current=this.head;
+        let previous;
+        let count=0;
+        while(count<index){
+            previous=current;
+            current=current.next;
+            count++;
+        }
+        this.size--;
+        previous.next=current.next
+    }
+
+    // PRINT THE WHOLE LIST
+    printLinkedList(){
+        let current=this.head;
+        while(current){
+            console.log("Node",current.data)
+            current=current.next;
+        }
+    }
+
+    // REMOVE DUPLICATE
+    removeDup(){
+        let prev=this.head;
+        let current=this.head.next;
+        let arr=[]
+        arr.push(this.head.data)
+        while(current){
+            if(arr.includes(current.data)){
+                prev.next=current.next;
+                
+            }
+            else{
+                arr.push(current.data)
+                prev=current;
+            }
+            
+            current=current.next;
+        }
+    }
+}
+
+l1=new LinkedList()
+
+l1.insertFirst(100)
+l1.insertLast(100)
+l1.insertLast(200)
+l1.insertLast(200)
+l1.insertLast(200)
+l1.insertLast(300)
+l1.insertLast(300)
+console.log("Before")
+l1.printLinkedList()
+
+l1.removeDup()
+console.log("After")
+l1.printLinkedList()
+
